406fc605452dee91ec818557efd830051280ca36
hvoking <gus@ugeom.com>
2024-09-15 05:23:21 -0400

    changed style url

--- src/components/main/maps/layers/tiles/index.tsx
+++ src/components/main/maps/layers/tiles/index.tsx
@@ -7,7 +7,7 @@ import { Source, Layer } from 'react-map-gl';
 export const Tiles = () => {
 	const { styleData, styleName } = useStyles();
 	
-	const url = `${process.env.REACT_APP_API_URL}/tiles/${styleName}?z={z}&x={x}&y={y}`;
+	const url = `${process.env.REACT_APP_API_URL}/tiles?schema_name=layers&style_name=${styleName}&z={z}&x={x}&y={y}`;
 
 	const layers = styleData.map((style: any, index: number) => {
 		return (
